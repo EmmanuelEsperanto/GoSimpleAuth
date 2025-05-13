58bc43e7c3110d636c73e067f903809baf622c7b
EmmanuelEsperanto <jobamhad@gmail.com>
2025-05-13 17:00:17 +0400

    Set base files

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"net/http"
+)
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page.")
+}
+
+func main() {
+	const connStr = "postgres://user:password@localhost:5432/dbname"
+	db, err := sql.Open("postgres", connStr)
+	// Регистрируем обработчики для разных маршрутов
+	fmt.Println(db.Stats())
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	// Запускаем сервер
+	fmt.Println("Starting server at port 8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
 }
